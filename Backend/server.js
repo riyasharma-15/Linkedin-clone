3639e7a543cfc7e786e0e07ad6ec85ecc9fc91f8
Riya Sharma <sharmariya2324@gmail.com>
2026-06-13 18:46:17 +0530

    Implement user profile post and connection schemas

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import postRoutes from "./routes/posts.routes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(postRoutes);
+
+app.use(express.json());
+
+const start = async () => {
+
+    const connectDB = await mongoose.connect("mongodb+srv://proconnect:clone@cluster0.puz1vx0.mongodb.net/")
+
+    app.listen(9080,() =>{
+        console.log("Server is listening on port 9080");
+    })
+
+}
+
+start();
